11692442e9883e626fbfd79eed3c82a92b94c318
Dhandapanikeerthana <keerthanadhandapani2005@gmail.com>
2024-11-12 15:46:10 +0530

    sucess

--- web.py
+++ web.py
@@ -4,12 +4,20 @@ content = """
 <html>
     <body>
         <h1 align="center">Device configuration</h1>
-        System type	64-bit operating system, x64-based processor</li>
+        <h2 align="center"> D Keerthana</h2>
+        <h3 align="center">(24900211)</h3>
+        <ol type="a">
+        <li>Device name	LAPTOP-55SHPGQA</li>
+         <li>Processor	12th Gen Intel(R) Core(TM) i5-1235U   1.30 GHz</li>
+         <li>Installed RAM	8.00 GB (7.68 GB usable)</li>
+          <li>Device ID	72061A66-1ACE-464E-8F25-30AD34620461</li>
+          <li>Product ID	00356-24670-67969-AAOEM</li>
+          <li>System type	64-bit operating system, x64-based processor</li>
            <li>Pen and touch	No pen or touch input is available for this display</li>
            </ol>
             
     </body>
-</html
+</html>
 """
 class myhandler(BaseHTTPRequestHandler):
     def do_GET(self):
@@ -21,12 +29,4 @@ class myhandler(BaseHTTPRequestHandler):
 server_address = ('',8000)
 httpd = HTTPServer(server_address,myhandler)
 print("my webserver is running...")
-httpd.serve_forever()<h2 align="center"> D Keerthana</h2>
-        <h3 align="center">(24900211)</h3>
-        <ol type="a">
-        <li>Device name	LAPTOP-55SHPGQA</li>
-         <li>Processor	12th Gen Intel(R) Core(TM) i5-1235U   1.30 GHz</li>
-         <li>Installed RAM	8.00 GB (7.68 GB usable)</li>
-          <li>Device ID	72061A66-1ACE-464E-8F25-30AD34620461</li>
-          <li>Product ID	00356-24670-67969-AAOEM</li>
-          <li>
+httpd.serve_forever()
